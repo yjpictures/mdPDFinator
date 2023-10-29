9a32acaca3021c95423122348f18f5a641402e37
Yash Jain <yashjain@mail.com>
2023-10-29 13:55:55 -0400

    update the options

--- mdPDFinator.py
+++ mdPDFinator.py
@@ -2,18 +2,18 @@ from markdown2 import markdown, markdown_path
 from weasyprint import HTML, CSS
 import fire
 
-def md2pdf(md_file_path, pdf_file_path='output.pdf', new_page_char="---", css_file_path=None):
+def md2pdf(md_file_path, output_file_path='output.pdf', new_page_char="---", style_file_path=None):
 	"""
 	This converts markdown to pdf
 
 	:param md_file_path: Path to the markdown file
 	:type md_file_path: str
-	:param pdf_file_path: Path to the output PDF file (it will not create a folder for you, make sure directory already exists)
-	:type pdf_file_path: str
-	:param new_page_char: Character for new page
+	:param output_file_path: Path to the output PDF file (it will not create a folder for you, make sure directory already exists)
+	:type output_file_path: str
+	:param new_page_char: Character for new page break
 	:type new_page_char: str
-	:param css_file_path: Path to the CSS file
-	:type css_file_path: str
+	:param style_file_path: Path to the CSS file
+	:type style_file_path: str
 	:return: Path of the converted PDF file
 	:rtype: str
 	"""
@@ -31,11 +31,11 @@ def md2pdf(md_file_path, pdf_file_path='output.pdf', new_page_char="---", css_fi
 		html_in_text = html_in_text.replace(page_break_char, '<p style="page-break-before: always" ></p>')
 	html_object = HTML(string=html_in_text)
 	css = []
-	if css_file_path:
-		css.append(CSS(filename=css_file_path))
-	html_object.write_pdf(pdf_file_path, stylesheets=css)
+	if style_file_path:
+		css.append(CSS(filename=style_file_path))
+	html_object.write_pdf(output_file_path, stylesheets=css)
 
-	return pdf_file_path
+	return output_file_path
 
 if __name__ == '__main__':
 	fire.Fire(md2pdf)
